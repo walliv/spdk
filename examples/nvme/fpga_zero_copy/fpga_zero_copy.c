f6d50a8ee9a23fb7969b89265bb79787c0107abd
Vladislav Valek <xvalek14@vutbr.cz>
2025-07-19 18:36:29 +0200

    fix(fpga_zero_copy): set LBA amount to 0
    Since LBA is a 0-based value, the value of 0 has to be written to
    register in order to read 1 LBA.

--- examples/nvme/fpga_zero_copy/fpga_zero_copy.c
+++ examples/nvme/fpga_zero_copy/fpga_zero_copy.c
@@ -478,7 +478,7 @@ static int dma_ctrl_init(struct ncd_probe_ctx *ncd_ctx, struct dma_ctrl_ctx *dma
 	nfb_comp_write64(dma_ctx->comp, REG_SQTDBL_BASE_ADDR, ncd_ctx->sqtdbl_paddr);
 	nfb_comp_write64(dma_ctx->comp, REG_CQHDBL_BASE_ADDR, ncd_ctx->cqhdbl_paddr);
 	nfb_comp_write64(dma_ctx->comp, REG_PRP_ENTRY_1_ADDR, ncd_ctx->data_bar_paddr);
-	nfb_comp_write16(dma_ctx->comp, REG_LBA_AMOUNT, 1);
+	nfb_comp_write16(dma_ctx->comp, REG_LBA_AMOUNT, 0);
 
 	return 0;
 
